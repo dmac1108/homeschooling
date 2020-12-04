95f0c23526ecd3cd37352e7d1460404f24f599df
Dawn McNeil <dawn.a.mcneil@gmail.com>
2020-12-03 20:52:59 -0500

    update test

--- test/homeschooling-service.spec.js
+++ test/homeschooling-service.spec.js
@@ -1,10 +1,37 @@
 require('dotenv').config()
 const { expect } = require("chai")
-const HomeschoolingService = require('../src/HomeschoolingService')
+const LessonsService = require('../src/lessons-service.js')
 const knex = require('knex')
 
-describe(`Homeschooling service object`, function(){
+describe(`Lessons service object`, function(){
     let db
+    
+    let testStudent = [{
+        student_id: 1,
+        firstname: 'Wade',
+        lastname: 'McNeil'
+
+    }];
+    
+    let testLessons = [{
+        date_completed: '2020-11-23',
+        school_subject: 'Math',
+        topic: 'Rounding Decimals',
+        lesson_type: 'In-Person Instruction',
+        reference: 'Teachers Pay Teachers',
+        student_id: 1
+    },
+    {
+        date_completed: '2020-11-23',
+        school_subject: 'Science',
+        topic: 'Weathering',
+        lesson_type: 'In-Person Instruction',
+        reference: 'Teachers Pay Teachers',
+        student_id: 1
+    }
+];
+
+
 
     before(()=>{
         db=knex({
@@ -13,10 +40,34 @@ describe(`Homeschooling service object`, function(){
         })
     })
     
+
     
+    after('disconnect from db', () => db.destroy())
+
+    before('clean the lessons table', () =>db('lessons').truncate())
+    before('clean the students table', ()=>db('students').truncate())
+    before('reset the serial number sequence', ()=>db('students').raw('select setval(pg_get_serial_sequence("students", "student_id"), COALESCE((SELECT MAX(student_id) +1 FROM students),1,false)'))
+    
+    before(()=>{
+        return db
+        .into('students')
+        .insert(testStudent)
+        
+    })
+
+    before(()=>{
+        return db
+        .into('lessons')
+        .insert(testLessons)
+    })
+
+
     describe(`getAllLessons()`,()=>{
         it(`resolves all lessons from 'lessons' table`, ()=>{
-            //test
+            return LessonsService.getAllLessons()
+            .then(actual => {
+                expect(actual).to.eql(testLessons);
+            })
         })
     })
 
