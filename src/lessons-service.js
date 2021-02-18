78a1e81eb076143e5c6f6b27d6678d1d09e7ea7a
Dawn McNeil <dawn.a.mcneil@gmail.com>
2021-02-17 20:20:25 -0500

    update lessons-service.spec.js to fix test errors

--- src/lessons-service.js
+++ src/lessons-service.js
@@ -1,6 +1,18 @@
 const LessonsService = {
     getAllLessons(knex){
         return knex.select('*').from('lessons');
-    }
+    },
+    insertLesson(knex, newLesson){
+        return knex
+        .insert(newLesson)
+        .into('lessons')
+        .returning('*')
+        .then(rows =>{
+            
+            return rows[0]
+            
+        })
+    },
+    
 }
 module.exports = LessonsService
--- test/lessons-service.spec.js
+++ test/lessons-service.spec.js
@@ -42,6 +42,8 @@ describe(`Lessons service object`, function(){
             connection: process.env.TEST_DB_URL
         })
     })
+
+   
     
 
     
@@ -51,18 +53,24 @@ describe(`Lessons service object`, function(){
     
     before('restart the student identity and truncate',() => db.raw('TRUNCATE students RESTART IDENTITY CASCADE'))
     
+    before(()=>{
+        return db
+        .into('students')
+        .insert(testStudent)
+        
+    })
     afterEach(()=> db('lessons').truncate())
-    afterEach('restart the student identity and truncate',() => db.raw('TRUNCATE students RESTART IDENTITY CASCADE'))
+    //afterEach('restart the student identity and truncate',() => db.raw('TRUNCATE students RESTART IDENTITY CASCADE'))
 
 
     context(`Given 'lessons' has data`, ()=>{
 
-        before(()=>{
+        /*before(()=>{
             return db
             .into('students')
             .insert(testStudent)
             
-        })
+        })*/
     
         before(()=>{
             return db
@@ -79,13 +87,40 @@ describe(`Lessons service object`, function(){
         })
     })
 
+    
     context(`Given 'lessons' has no data`, ()=>{
+        
         it(`getAllLessons() returns an empty array`, ()=>{
             return LessonsService.getAllLessons(db)
             .then(actual => {
                 expect(actual).to.eql([])
             })
         })
+
+        it(`insertLesson'() inserts a new lesson and resolves the new lesson with an 'id'`, ()=>{
+
+            const newLesson = {
+                date_completed: new Date('2020-12-13T16:28:32.615Z'),
+                school_subject: 'Math',
+                topic: 'Fractions',
+                lesson_type: 'In-Person Instruction',
+                reference: 'Teachers Pay Teachers',
+                student_id: 1
+            }
+
+            return LessonsService.insertLesson(db, newLesson)
+            .then(actual =>{
+                expect(actual.lesson_id).to.eql(1)
+                expect(actual.date_completed).to.eql(new Date(newLesson.date_completed))
+                expect(actual.school_subject).to.eql(newLesson.school_subject)
+                expect(actual.topic).to.eql(newLesson.topic)
+                expect(actual.lesson_type).to.eql(newLesson.lesson_type)
+                expect(actual.reference).to.eql(newLesson.reference)
+                expect(actual.student_id).to.eql(newLesson.student_id)
+                
+            })
+        } )
+
     })
 
 })
